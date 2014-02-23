66a3351582f8d7b84312aae69fbceca71797fabe
Guy Sopher <guysopher@gmail.com>
2014-02-23 23:37:51 +0200

    generator

---
+++ app/scripts/services/instagram.js
@@ -0,0 +1,24 @@
+'use strict';
+
+angular.module('clientApp')
+.factory('Instagram', ['$http',
+    function($http) {
+        var base = "https://api.instagram.com/v1";
+        var clientId = 'd9c1142d0ac14d1ea5a45bc8478006a4';
+        var hashtag = "treatsforlife"
+        return {
+            'get': function(count) {
+                var request = '/tags/' + hashtag + '/media/recent';
+                var url = base + request;
+                var config = {
+                    'params': {
+                        'client_id': clientId,
+                        'count': count,
+                        'callback': 'JSON_CALLBACK'
+                    }
+                };
+                return $http.jsonp(url, config);
+            }
+        };
+    }
+]);
