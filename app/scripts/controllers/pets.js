71aab44345f4932aaeb05b08bda0bfc5739b44c4
Guy Sopher <guysopher@gmail.com>
2014-02-25 00:28:19 +0200

    swipe! hooray!!!

--- app/scripts/controllers/pets.js
+++ app/scripts/controllers/pets.js
@@ -9,6 +9,20 @@ angular.module('clientApp')
             $timeout(function(){
                 $scope.photos = res.data;
                 //todo - paging
-            })
+            });
+            $timeout(function(){
+                window.mySwipe = new Swipe(document.getElementById('slider'), {
+                    startSlide: 2,
+                    speed: 400,
+                    auto: 3000,
+                    continuous: true,
+                    disableScroll: false,
+                    stopPropagation: false,
+                    callback: function(index, elem) {},
+                    transitionEnd: function(index, elem) {}
+                });
+            },1000);
+
         });
+
     });
