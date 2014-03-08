23af6468e016a267881f9a192fbfe7a2a568eb07
Guy Sopher <guysopher@gmail.com>
2014-03-08 17:40:04 +0200

    swipeview active

--- app/scripts/controllers/pets.js
+++ app/scripts/controllers/pets.js
@@ -30,28 +30,55 @@ angular.module('clientApp')
 
         $rootScope.bodyClass='pets';
         $scope.grassHeight = angular.element(window).height() - angular.element(window).width();
+        $scope.screenWidth = angular.element(window).width();
 
         Instagram.get(100).success(function (res) {
             $timeout(function(){
                 $scope.photos = res.data;
             });
             $timeout(function(){
-                window.videosSwipe = new Swipe(document.getElementById('slider'), {
-                    startSlide: 0,
-                    continuous: true,
-                    disableScroll: true,
-                    stopPropagation: false,
-                    callback: function(index, elem) {},
-                    transitionEnd: function(index, elem) {}
+                $scope.slides = [];
+                for (var p in $scope.photos){
+                    var photo = $scope.photos[p];
+                    var slide = {
+                        img: photo.images.standard_resolution.url,
+                        width: $scope.screenWidth,
+                        height: $scope.screenWidth
+                    }
+                    $scope.slides.push(slide);
+                }
+                var gallery = new SwipeView('#pet-pic', { numberOfPages: $scope.slides.length });
+                for (var i=0; i<3; i++) {
+                    var page = i==0 ? $scope.slides.length-1 : i-1;
+                    var el = document.createElement('img');
+                    el.className = 'loading';
+                    el.src = $scope.slides[page].img;
+                    el.width = $scope.slides[page].width;
+                    el.height = $scope.slides[page].width;
+                    el.onload = function () { this.className = ''; }
+                    gallery.masterPages[i].appendChild(el);
+                }
+                gallery.onFlip(function () {
+                    var el, upcoming;
+                    for (var i=0; i<3; i++) {
+                        upcoming = gallery.masterPages[i].dataset.upcomingPageIndex;
+                        if (upcoming != gallery.masterPages[i].dataset.pageIndex) {
+                            el = gallery.masterPages[i].querySelector('img');
+                            el.className = 'loading';
+                            el.src = $scope.slides[upcoming].img;
+                        }
+                    }
                 });
-                window.treatsSwipe = new Swipe(document.getElementById('slider-treats'), {
-                    startSlide: 0,
-                    continuous: true,
-                    disableScroll: true,
-                    stopPropagation: false,
-                    callback: function(index, elem) {},
-                    transitionEnd: function(index, elem) {}
+                gallery.onMoveOut(function () {
+                    gallery.masterPages[gallery.currentMasterPage].className = gallery.masterPages[gallery.currentMasterPage].className.replace(/(^|\s)swipeview-active(\s|$)/, '');
                 });
+                gallery.onMoveIn(function () {
+                    var className = gallery.masterPages[gallery.currentMasterPage].className;
+                    /(^|\s)swipeview-active(\s|$)/.test(className) || (gallery.masterPages[gallery.currentMasterPage].className = !className ? 'swipeview-active' : className + ' swipeview-active');
+                });
+
+
+
             },1000);
         });
 
