3ca6241494668e94ddad38312b27ffd083c72da5
Guy Sopher <guysopher@gmail.com>
2014-03-20 00:50:26 +0200

    working on dialog

---
+++ app/scripts/directives/tipdialog.js
@@ -0,0 +1,12 @@
+'use strict';
+
+angular.module('clientApp')
+  .directive('tipDialog', function () {
+    return {
+      templateUrl: 'partials/tipDialog.html',
+      restrict: 'E',
+      link: function postLink(scope, element, attrs) {
+        scope.text = ('this is the tipDialog directive');
+      }
+    };
+  });
