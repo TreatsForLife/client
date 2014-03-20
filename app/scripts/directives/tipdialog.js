9a12021f5e524d73cfe25d00e6ae9601c5cb3ad8
Guy Sopher <guysopher@gmail.com>
2014-03-20 02:10:25 +0200

    animation of dialog

--- app/scripts/directives/tipdialog.js
+++ app/scripts/directives/tipdialog.js
@@ -1,15 +1,29 @@
 'use strict';
 
 angular.module('clientApp')
-  .directive('tipDialog', function () {
+  .directive('tipDialog', function ($timeout) {
     return {
       templateUrl: 'partials/tipDialog.html',
       restrict: 'E',
       link: function postLink(scope, element, attrs) {
-        scope.text = ('this is the tipDialog directive');
+
+          var texts = {
+              buy_treat: "איזה כיף שהחלטת לקנות לי מתנה!\n" +
+                  ""
+          }
+
+
+        scope.text = texts.buy_treat;
 
           scope.showTipDialog = function(){
               scope.shown = true;
+          }
+          scope.closeTipDialog = function(){
+
+              scope.leaving = true;
+              $timeout(function(){
+                  scope.shown = false;
+              },1000);
           }
       }
     };
