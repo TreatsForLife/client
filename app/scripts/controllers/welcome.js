507ccabdc3b7edea212b02ecdb26195cba3c0387
Guy Sopher <guysopher@gmail.com>
2014-06-28 14:53:40 +0300

    welcome complete

--- app/scripts/controllers/welcome.js
+++ app/scripts/controllers/welcome.js
@@ -1,24 +1,34 @@
 'use strict';
 
 angular.module('clientApp')
-  .controller('WelcomeCtrl', function ($scope, $rootScope, $location) {
-        $rootScope.bodyClass='welcome';
-        $scope.fbLogin = function(){
+    .controller('WelcomeCtrl', function ($scope, $rootScope, $timeout) {
+        $rootScope.bodyClass = 'welcome';
+
+        $scope.placeLogo = function () {
+            $timeout(function () {
+                $scope.logoSpace = $('.welcome-app-explained').offset().top;
+                $scope.logoMargin = (($scope.logoSpace - 196) / 2) + 'px auto';
+            });
+        }
+
+        $scope.fbLogin = function () {
             if (!FB) return;
 //            localStorage.setItem('fb', true);
 //            console.log(localStorage);
 //            $location.path('/');
-            FB.login(function(response) {
+            FB.login(function (response) {
                 if (response.authResponse) {
-                    console.log('Welcome!  Fetching your information.... ');
-                    FB.api('/me', function(response) {
+//                    console.log('Welcome!  Fetching your information.... ');
+                    FB.api('/me', function (response) {
                         localStorage.setItem('fb', response);
-                        console.log('Good to see you, ' + response.name + '.');
+                        $location.path('/');
+//                        console.log('Good to see you, ' + response.name + '.');
                     });
                 } else {
                     console.log('User cancelled login or did not fully authorize.');
                 }
             });
-
         }
-  });
+
+        window.debug = $scope;
+    });
