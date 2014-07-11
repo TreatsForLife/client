0c32ae5ce50146888ddb8fb25c3443e2e78dde41
Guy Sopher <guysopher@gmail.com>
2014-07-12 01:00:56 +0300

    nganimate caused bower update

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -5,6 +5,7 @@ angular.module('clientApp', [
         'ngResource',
         'ngSanitize',
         'ngRoute',
+        'ngAnimate',
         'timer'
     ])
     .config(function ($routeProvider, $compileProvider) {
