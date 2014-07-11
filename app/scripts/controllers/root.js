96c5073b97328ddcccb66ab01cac0e6151b2c7eb
Guy Sopher <guysopher@gmail.com>
2014-07-12 01:19:59 +0300

    page transitions

--- app/scripts/controllers/root.js
+++ app/scripts/controllers/root.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('clientApp')
-    .controller('RootCtrl', function ($scope, $rootScope) {
+    .controller('RootCtrl', ['$scope', '$rootScope', '$timeout', function ($scope, $rootScope, $timeout) {
         $scope.instajam = Instajam.init({
             clientId: '1e7c407bd22b435e8447b7607245a947',
             redirectUri: 'http://127.0.0.1:9000',
@@ -13,7 +13,11 @@ angular.module('clientApp')
         $rootScope.picHeight = $(window).width() * 0.7;
 
         $rootScope.goBack = function () {
+            $scope.goingBack = true;
+            $timeout(function(){
+                $scope.goingBack = false;
+            },1000);
             window.history.back();
         }
 
-    });
+    }]);
