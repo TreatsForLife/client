257f948e9e803dd5fc2ce8b1a87453ee69f25e02
Guy Sopher <guysopher@gmail.com>
2014-07-12 12:33:09 +0300

    basic shop css

--- app/scripts/controllers/shop.js
+++ app/scripts/controllers/shop.js
@@ -3,5 +3,6 @@
 angular.module('clientApp')
     .controller('ShopCtrl', ['$scope', '$rootScope', 'Treats', function ($scope, $rootScope, Treats) {
         $rootScope.bodyClass = 'shop';
+        $rootScope.navbarTitle = 'החנות';
         $scope.treats = Treats.all();
     }]);
