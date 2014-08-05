038f98a7686af660d4d8e98c83c6098d6dd62a53
Guy Sopher <guysopher@gmail.com>
2014-08-06 00:08:50 +0300

    IONIC
    Squashed commit of the following:
    
    commit 139cb52e7905c5fe72d4b52267db49dbacfcccb7
    Author: Guy Sopher <guysopher@gmail.com>
    Date:   Wed Aug 6 00:05:29 2014 +0300
    
        ionic success
    
    commit 700c64ac1b7b0586630d2d3e2ccc83ac2c30d345
    Author: Guy Sopher <guysopher@gmail.com>
    Date:   Wed Aug 6 00:05:11 2014 +0300
    
        ionic success
    
    commit a01eff657389f73ef481d2920c94597793d05c5f
    Author: Guy Sopher <guysopher@gmail.com>
    Date:   Wed Aug 6 00:02:51 2014 +0300
    
        grunt build
    
    commit eb68ebe34c54686f681cc542c23484b5f35ad874
    Author: Guy Sopher <guysopher@gmail.com>
    Date:   Tue Aug 5 23:52:08 2014 +0300
    
        grunt build
    
    commit fda11e6a0a24db25987a5215c64e2526799e8bc0
    Author: Guy Sopher <guysopher@gmail.com>
    Date:   Tue Aug 5 23:49:13 2014 +0300
    
        test logo
    
    commit d4ff98376b9a20b751b5b8c5336d85df3b98c20d
    Author: Guy Sopher <guysopher@gmail.com>
    Date:   Tue Aug 5 23:48:17 2014 +0300
    
        add ionic

---
+++ ionic/platforms/android/ant-build/TreatsForLife-debug-unaligned.apk.d
@@ -0,0 +1,4 @@
+/home/guy/Projects/TreatsForLife/client/ionic/platforms/android/ant-build/TreatsForLife-debug-unaligned.apk : \
+/home/guy/Projects/TreatsForLife/client/ionic/platforms/android/ant-build/TreatsForLife.ap_ \
+/home/guy/Projects/TreatsForLife/client/ionic/platforms/android/ant-build/classes.dex \
+/home/guy/Projects/TreatsForLife/client/ionic/platforms/android/CordovaLib/ant-build/classes.jar \
