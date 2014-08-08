542b21e0f2fd59d1d713a182e016a5a364ba8e8c
Guy Sopher <guysopher@gmail.com>
2014-08-08 23:15:05 +0300

    ionic + facebook plugin wooohhhaaa!!

---
+++ ionic/platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/home/guy/Projects/TreatsForLife/client/ionic/platforms/android/CordovaLib/ant-build/AndroidManifest.xml : \
+/home/guy/Projects/TreatsForLife/client/ionic/platforms/android/CordovaLib/AndroidManifest.xml \
---
+++ ionic/platforms/android/FacebookLib/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/home/guy/Projects/TreatsForLife/client/ionic/platforms/android/FacebookLib/bin/AndroidManifest.xml : \
+/home/guy/Projects/TreatsForLife/client/ionic/platforms/android/FacebookLib/AndroidManifest.xml \
